b308bfb1b3a23972b27a762bb478d5f6c8ffaf30
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-09 22:57:58 +0430

    change componenet to funcion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Greeting from "./components/Greeting";
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Greeting />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
